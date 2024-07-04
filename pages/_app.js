73e5c6fe92c365ccd00d40335f233ac6c1763e48
Samuel <s.sjoblom@gmail.com>
2024-07-04 15:11:25 +0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '@styles/globals.scss'
 import { Inter_Tight } from 'next/font/google'
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+
+
 
 // If loading a variable font, you don't need to specify the font weight
 const inter_tight = Inter_Tight({
@@ -9,7 +13,7 @@ const inter_tight = Inter_Tight({
 
 function Application({ Component, pageProps }) {
   return (
-    <main className={inter_tight.className}>
+    <main className={GeistSans.className}>
       <Component {...pageProps} />
     </main>
   )
--- pages/index.js
+++ pages/index.js
@@ -12,28 +12,18 @@ export default function Home() {
 
         <div className='container-fluid my-5 pt-5'>
           <div className='row'>
-            <p className='xxl medium mt-3'>Hello, I'm Samuel.</p>
-            <h1>I create <span className='highlight'>art</span> and design digital products with focus on <span className='highlight'>visual design.</span>
-            </h1>
+            <h3>Hi.</h3>
+            <p className='xxl light'>I work with UX & Graphic design <a href='http://djakne.com'>here</a>. In my spare time I like to play atound with various creative projects. At the moment I am learning to how to photograph.</p>
             <div className='d-flex justify-content-between align-items-center my-5'>
-            <a className='btn btn-primary me-2 btn-lg' href="mailto:s.sjoblom@gmail.com">Let’s Talk</a>
-            <p className='xl mt-3 col-6'>I'm a multidisciplinary designer comfortable both front of the frontline in the code and as an illustrator.</p>
+              <div>
+              <a className='btn btn-primary me-2 btn-lg' href="mailto:s.sjoblom@gmail.com">Contact</a>
+              <a className='btn btn-primary me-2 btn-lg' href="https://www.instagram.com/snabb.studio/">Instagram</a>
+              </div>
+              <p className='xl mt-3 col-6'></p>
             </div>
-          </div>
-        </div>
-        <div className='container-fluid my-5 pt-5'>
-          <div className='row'>
-            <h5>Selected works
-            </h5>
-            <div className='d-flex justify-content-between align-items-center my-5 border-bottom'>
-              <img src="./sauna-02.png" class="img-fluid" alt="..."/>
-            </div>
-            <h6 className='xl medium'>"Sauna"</h6>
-            <p>A personal project with the ain to create fresh sauna material.</p>
           </div>
         </div>
       </main>
-      <Footer />
     </div>
   )
 }
