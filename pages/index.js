9f08781bb1741e60c0a33454816e033adc64de39
Samuel <s.sjoblom@gmail.com>
2026-04-09 10:01:05 +0200

    v1 of new web done

--- pages/index.js
+++ pages/index.js
@@ -9,36 +9,22 @@ const imageColumns = [
     "7RV00648.jpg",
     "7RV00359.jpg",
     "7RV00064.jpg",
-    "A7C00127-HDR.jpg",
-    "A7C00699.jpg",
     "A7C00672.jpg",
-    "A7C00885.jpg",
     "A7C00427.jpg",
-    "A7C00410.jpg",
     "A7C00023.jpg",
-    "A7C00138.jpg",
     "A7C00267.jpg",
-    "A7C00326.jpg",
     "A7C00641.jpg",
-    "A7C01079.jpg",
   ],
   [
-    "A7C01198.jpg",
+    "7RV00537.jpg",
     "A7C00375.jpg",
     "A7C00015-HDR.jpg",
-    "A7C00121.jpg",
-    "A7C00133.jpg",
-    "A7C00248.jpg",
     "A7C00195.jpg",
     "A7C00289.jpg",
-    "A7C00621.jpg",
-    "A7C00639.jpg",
-    "A7C00651.jpg",
+
     "A7C00844.jpg",
   ],
   [
-    "A7C01280.jpg",
-    "A7C00260.jpg",
     "A7C00017.jpg",
     "A7C00633.jpg",
     "A7C00790.jpg",
@@ -46,7 +32,6 @@ const imageColumns = [
     "A7C00298.jpg",
     "A7C00627.jpg",
     "A7C00785.jpg",
-    "A7C01333.jpg",
   ],
 ];
 
@@ -107,7 +92,6 @@ export default function Home() {
         <script type="application/ld+json">{`{"@context": "https://schema.org", "@type": "Person", "name": "Snabb Studio", "alternateName": "snabb.studio", "jobTitle": "Photographer / Artist", "description": "photography with the intention to make something you feel not something you understand", "url": "https://snabb.studio", "sameAs": ["https://www.instagram.com/snabb.studio"]}`}</script>
       </Head>
 
-      {/* Normal scrolling with GSAP smooth scroll */}
       <div>
         <main className="d-flex justify-content-start align-items-start flex-column">
           {/* Gallery */}
@@ -139,34 +123,12 @@ export default function Home() {
             </div>
           </div>
 
-          {/* Footer info - scrolls with content */}
-          <div className="container-fluid custom-max-width">
-            <div className="row justify-content-end py-5">
-              <div className="col-9">
-                <h4 className="pt-5">
-                  This site is like me — slow to update.{" "}
-                  <a href="https://www.instagram.com/snabb.studio/">
-                    Instagram
-                  </a>{" "}
-                  is quicker, but only a little.
-                </h4>
-              </div>
-            </div>
-            <div className="row justify-content-start py-5 mb-5">
-              <div className="col-7 mb-5">
-                <h4 className="pt-5 mb-5">
-                  <a href="mailto:s.sjoblom@gmail.com">Send me a message.</a>{" "}
-                  I'll probably answer faster than a toy car in the fog.
-                </h4>
-              </div>
-            </div>
-          </div>
 
           {/* Copyright */}
-          <div className="container-fluid text-center py-3">
+          <div className="container-fluid text-center py-3 pt-4">
             <div className="row justify-content-center">
               <p className="text-center s">
-                © 2025 Snabb Studio. All photos are mine, not yours. You may
+                © 2026 Snabb Studio. All photos are mine, not yours. You may
                 look, scroll, and admire — but don't copy, steal, or use them
                 for your own projects without asking. If you want to license an
                 image, <a href="mailto:s.sjoblom@gmail.com">contact me</a>.
@@ -176,28 +138,20 @@ export default function Home() {
         </main>
       </div>
 
-      {/* Fixed header - logo and toggle stay on top */}
+
       <div
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          right: 0,
-          zIndex: 1000,
-          pointerEvents: "none",
-        }}
       >
-        <h6 className="pt-5 hero-subtitle">
-                photography with the intention to make something you feel not
-                something you understand
-              </h6>
         <div className="flex-container mb-5 custom-max-width">
           <div className="row">
-            <div className="hero fixed-center w-80">
-              <img src="/logo.svg" className="img-fluid w-100" alt="" />
-              
+            <div className="hero fixed-center w-80 mb-5 pb-5">
+              <img src="/logo.svg" className="img-fluid w-100 mb-5 pb-5" alt="" />
             </div>
-            <div
+            <div className="fixed-center w-80 blend-difference mt-5 pt-5">
+              <h6 className="pt-5 mt-5 hero-subtitle">
+                with the intention to make photos that feel like something
+              </h6>
+            </div>
+{/*             <div
               className="layout-toggle mb-4 fixed-center"
               style={{ pointerEvents: "auto" }}
             >
@@ -228,7 +182,7 @@ export default function Home() {
                   </span>
                 </span>
               </button>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
